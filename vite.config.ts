6a36c8d0044d9f6bfca8a6b76fefc383f22cfe09
ChiuChuiJen <107801364+ChiuChuiJen@users.noreply.github.com>
2026-01-06 22:15:33 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  // 設定為您的 GitHub Repository 名稱，前後都要有斜線
+  // 如果您的網址是 chiuchuijen.github.io/BDO，這裡就是 '/BDO/'
+  base: '/BDO/', 
 })
